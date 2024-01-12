c392b9a19ffcb9ee7cd26fe266f5fef103abcf13
iisaacc <isporras@student.42malaga.com>
2024-01-12 17:31:02 +0100

    libft

--- includes/minishell.h
+++ includes/minishell.h
@@ -13,9 +13,12 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
 
 typedef struct s_lexer
 {
