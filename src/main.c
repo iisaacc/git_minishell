429f4073413c27004902cb1b4efcf5b9ead24f0c
cadufrederiksen <cadu.riksen@gmail.com>
2024-01-16 17:02:38 +0100

    stuff add

--- src/main.c
+++ src/main.c
@@ -6,11 +6,68 @@
 /*   By: carmarqu <carmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 14:19:24 by carmarqu          #+#    #+#             */
-/*   Updated: 2024/01/16 13:57:25 by carmarqu         ###   ########.fr       */
+/*   Updated: 2024/01/16 16:57:44 by carmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+int	ft_countl_mod(char const *s, char c)
+{
+	int	i;
+	int	count;
+	int	b;
+
+	b = 0;
+	i = 0;
+	count = 0;
+	while (s && s[i] && count < 2)
+	{
+		if (s[i] != c && b == 0 && count < 2)
+		{
+			count++;
+			b = 1;
+		}
+		if (s[i] == c)
+			b = 0;
+		i++;
+	}
+	return (count);
+}
+
+char	**ft_splitaux_mod(char **str, char *s, char c)
+{
+	int		i;
+	int		len;
+	int		lap;
+
+	lap = 0;
+	i = 0;
+	while (s && s[i])
+	{
+		len = 0;
+		while (s[i + len] != c && s[i + len] != '\0')
+			len++;
+		if (len > 0)
+			ft_to2d(&s[i], len, str, lap++);
+		i += len;
+		c = '\0';
+		if (s[i] != '\0')
+			i++;
+	}
+	str[lap] = NULL;
+	return (str);
+}
+
+char	**ft_split_mod(char const *s, char c)
+{
+	char	**ans;
+
+	ans = (char **)malloc((ft_countl_mod(s, c) + 1) * sizeof(char *));
+	if (!ans)
+		return (NULL);
+	return (ft_splitaux_mod(ans, (char *)s, c));
+}
 
 int	main(int argc, char **argv)
 {
@@ -26,7 +83,8 @@ int	main(int argc, char **argv)
 	while((input = readline("Minishell>")))
 	{
 		add_history(input);
-		lexer(input);
+		//lexer(input);
+		ft_split_mod(input, ' ');
 		printf("%s\n", input);
 		//rl_replace_line(" ", 0);
 	}
