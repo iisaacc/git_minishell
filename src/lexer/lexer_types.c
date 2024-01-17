8fa87dcfa4d66758d1adeb662d3a1959c72a18eb
cadufrederiksen <cadu.riksen@gmail.com>
2024-01-17 17:11:31 +0100

    tyes add

--- src/lexer/lexer_types.c
+++ src/lexer/lexer_types.c
@@ -6,14 +6,19 @@
 /*   By: carmarqu <carmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 15:13:24 by carmarqu          #+#    #+#             */
-/*   Updated: 2024/01/17 16:50:37 by carmarqu         ###   ########.fr       */
+/*   Updated: 2024/01/17 17:10:46 by carmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-void	ft_subtypes()
+void	ft_subtypes(t_lexer *aux, int *flag, int *file)
 {
+	if (aux->word[0] == '|')
+		*flag = 1;
+	if ((aux->word[0] == '>' || aux->word[0] == '<') && aux->word[1] != '<')
+		*file = 1;
+	aux->type = 3;
 	
 } 
 
@@ -21,7 +26,9 @@ void	ft_types(t_lexer *lexer)
 {
 	t_lexer *aux;
 	int flag; //flag para saver quando tem um pipe
+	int file;
 	
+	file = 0;
 	flag = 1;
 	aux = lexer;
 	while (aux)
@@ -34,11 +41,9 @@ void	ft_types(t_lexer *lexer)
 		else if (aux->word[0] == '-')//es una flag
 			aux->type = 2;
 		else if (aux->word[0] == '|' || aux->word[0] == '<' || aux->word[0] == '>')//es um token
-		{
-			if (aux->word[0] == '|' )
-				flag++;
-			aux->type = 5;
-		}
+			ft_subtypes(aux, &flag, &file);
+		else	
+			aux->type = 4;
 		aux = aux->next;
 	}
 }
