578f7af953f5c9edf0d51cca71b73204167d2edc
cadufrederiksen <cadu.riksen@gmail.com>
2024-01-17 14:33:55 +0100

    free list

--- src/lst_init.c
+++ src/lst_init.c
@@ -6,12 +6,27 @@
 /*   By: carmarqu <carmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 12:35:16 by carmarqu          #+#    #+#             */
-/*   Updated: 2024/01/17 14:00:30 by carmarqu         ###   ########.fr       */
+/*   Updated: 2024/01/17 14:33:25 by carmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+void	free_node(t_lexer **node)
+{
+	t_lexer	*node_aux;
+
+	if (!node | !*node)
+		return ;
+	while (*node)
+	{
+		node_aux = (*node)->next;
+		free((*node)->word);
+		free(*node);
+		*node = node_aux;
+	}
+	*node = NULL;
+}
 
 t_lexer	*last_list(t_lexer *lst)//va hasta el final de la lista
 {
@@ -37,9 +52,8 @@ void	add_new(t_lexer **lst, t_lexer *new)//añande un nodo a la lista
 	aux->next = new;
 }
 
-t_lexer *create_new(char *input)
+t_lexer *create_new(char *input, int x)
 {
-	static int i = 1;
 	t_lexer *node;
 	
 	node = NULL;
@@ -47,8 +61,7 @@ t_lexer *create_new(char *input)
 	node->word = ft_substr(input, 0, ft_strlen(input));
 	node->next = NULL;
 	node->type = 0;
-	node->id = i;
-	i++;
+	node->id = x + 1;
 	return (node);
 }
 
@@ -57,10 +70,11 @@ void ast(t_lexer **lexer, char **input)//crea todos los nudos
 	int x;
 	
 	x = 0;
-	//printf("aqui\n");
+	if(*lexer)
+		free_node(lexer);
 	while (input[x])
 	{
-		add_new(lexer, create_new(input[x]));
+		add_new(lexer, create_new(input[x], x));
 		x++;
 	}
 }
