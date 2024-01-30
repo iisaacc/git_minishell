29fedcc9aee4cd5116aa81f6c20fad6d87a0ed77
iisaacc <isaacporras2001@gmail.com>
2024-01-30 15:07:34 +0100

    ahra si

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: isporras <isporras@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 11:52:20 by isporras          #+#    #+#             */
-/*   Updated: 2024/01/30 15:06:49 by isporras         ###   ########.fr       */
+/*   Updated: 2024/01/30 15:07:19 by isporras         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,30 @@ void	ft_set_io(t_mini **mini, t_lexer **lexer)
 	t_lexer	*aux;
 	t_mini	*m_node;
 
-	aux = *lexer;
-	i = 0;
-	while (aux)
+	m_node = *mini;
+	lap = 0;
+	while (m_node)
 	{
-		if (aux->type == PIPE)
-			i++;
-		if (aux->type == LESS && i == lap)
-			m_node->infile = open((aux->next)->word, O_RDONLY);
-		if (aux->type == GREATER && i == lap)
-			m_node->outfile = open((aux->next)->word, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		aux = aux->next;
-	}
-	if (m_node->infile == -1 || m_node->outfile == -1)
-	{
-		ft_putstr_fd("Error: ", 2);
-		ft_putstr_fd(strerror(errno), 2);
-		ft_putstr_fd("\n", 2);
+		i = 0;
+		aux = *lexer;
+		while (aux)
+		{
+			if (aux->type == PIPE)
+				i++;
+			if (aux->type == LESS && i == lap)
+				m_node->infile = open((aux->next)->word, O_RDONLY);
+			if (aux->type == GREATER && i == lap)
+				m_node->outfile = open((aux->next)->word, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			aux = aux->next;
+		}
+		if (m_node->infile == -1 || m_node->outfile == -1)
+		{
+			ft_putstr_fd("Error: ", 2);
+			ft_putstr_fd(strerror(errno), 2);
+			ft_putstr_fd("\n", 2);
+		}
+		m_node = m_node->next;
+		lap++;
 	}
 }
 
